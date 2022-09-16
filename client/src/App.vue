1c6bdf0961a885c6723aeeadfc28ece30554a18a
Iskandar Agung <iskand.agung@gmail.com>
2022-09-16 07:37:20 +0700

    done

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,3 @@
-<script></script>
-
-<template></template>
+<template>
+  <router-view></router-view>
+</template>
---
+++ client/src/views/NotFoundView.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  created() {
+    setTimeout(() => {
+      this.$router.push({ name: "home" });
+    }, 2000);
+  },
+};
+</script>
+
+<template>
+  <div class="columns is-full">
+    <div class="column is-full m-6">
+      <h1 class="title">404</h1>
+      <h2 class="subtitle">Not Found</h2>
+    </div>
+  </div>
+</template>
